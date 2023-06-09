d6ba9798bb87e0cc27564c2dbbf7b9eaa9e9c264
Aayush Kumar Shukla <56974422+AayushKrShukla@users.noreply.github.com>
2023-06-09 21:09:38 +0530

    added partials and refactored code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -11,8 +11,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+  end
+  
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -22,13 +25,8 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -37,8 +35,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
+    flash[:notice] = "Article was deleted successfully"
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id]) 
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
