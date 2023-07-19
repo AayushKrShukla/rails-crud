d005bd97b507b65d2af1a86f30f5193c14ebceba
Aayush Kumar Shukla <56974422+AayushKrShukla@users.noreply.github.com>
2023-07-19 19:33:16 +0530

    added ability to edit category and updated layout to show categories with articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,25 @@ class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @category = Category.new
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category was updated successfully"
+      redirect_to @category
+    else
+      render "edit"
+    end
   end
 
   def create
